9ddb7ae26e29d0cdea33654f91ff02fb1f6f9fde
Maxim <kmaxusk@gmail.com>
2025-02-26 21:42:17 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -3,12 +3,9 @@ class Node:
         self.data = data
         self.next = None
 
-
 class LinkedList:
     def __init__(self):
         self.head = None
-
-         
 
     def insert_at_beginning(self, data):
         new_node = Node(data)
@@ -93,120 +90,52 @@ class LinkedList:
         return slow
     
     def sort(self):
-        if self.head is None or self.head.next is None:
-        # if len(self) <= 1:
+        if self.head is None or self.head.next is None:        
             return self
 
-        mid = self.get_middle()
-        
-        cur = self.head
+        mid = self.get_middle()        
+        cur = self.head        
         left_half = LinkedList()
-        left_half.head = self.head
-        # left_current = left_half.head
+        left_half.head = self.head        
         print("Inside sort")
-        print(left_half.head.data)
-        while cur.next is not None and cur.next.data != mid.data:
+        print("Left head", left_half.head.data)
+        while cur.next is not None and not cur.next.data == mid.data:            
             left_half.next = cur.next
-            cur = cur.next
-            print(left_half.next.data)
-        # left_half = arr[:mid]
-        # right_half = arr[mid:]
-        # return merge(merge_sort(left_half), merge_sort(right_half))
+            cur = cur.next            
+            print("Left element", left_half.next.data)        
         left_half.next = None
-        print()
-        print(cur.next.data)
-
-        print()
-
-        right_half = LinkedList()
-        right_half.head = cur
+        print("Element after left end:", left_half.next)        
+        print("Middle", cur.next.data, "=", mid.data)        
+        cur = cur.next        
+        right_half = LinkedList()        
+        right_half.head = cur        
+        print("Right head", right_half.head.data)
         while cur.next is not None:
             right_half.next = cur.next
             cur = cur.next
-            print(cur.data)
+            print("Right element", cur.data)
         
         print()
 
-        print("Lists")
+        print("Left list")        
         left_half.print_list()
-        print()
+        print("Right list")
         right_half.print_list()
 
-        return right_half
+        return left_half
     
 
-    # def sort(self):
-    #     i = 1
-    #     current = self.head        
-    #     while current.next.next:
-    #         tmp = current.next
-    #         # current = current.next
-    #         j = i - 1
-    #         current = self.head
-    #         while j >= 0:                
-    #             prev_node = current
-    #             current_node = current.next
-    #             if prev_node.value > current_node.value:
-    #                 prev_node.value = current_node.value
-    #             j -= 1
-            
-
-
-                # tmp.data < current.next.next.data:
-            # print(current.next.next.data)
-                
-            
-
-            
-        # for i in range(1, len(lst)):
-        #     key = lst[i]
-        #     j = i-1
-        #     while j >=0 and key < lst[j] :
-        #         lst[j+1] = lst[j]
-        #         j -= 1
-        #     lst[j+1] = key 
-
-
-        
-
-
-
-
 llist = LinkedList()
-
-
-# Вставляємо вузли в початок
 llist.insert_at_beginning(5)
 llist.insert_at_beginning(10)
 llist.insert_at_beginning(15)
-
-# Вставляємо вузли в кінець
 llist.insert_at_end(20)
 llist.insert_at_end(25)
 
-# Друк зв'язного списку
 print("Зв'язний список:")
 llist.print_list()
 
-llist.reverse()
 print()
-llist.print_list()
-print()
-print("Left")
-
 l = llist.sort()
-print()
+print("Left list")
 l.print_list()
-# llist.sort()
-
-# # Видаляємо вузол
-# llist.delete_node(10)
-
-# print("\nЗв'язний список після видалення вузла з даними 10:")
-# llist.print_list()
-
-# # Пошук елемента у зв'язному списку
-# print("\nШукаємо елемент 15:")
-# element = llist.search_element(15)
-# if element:
-#     print(element.data)
